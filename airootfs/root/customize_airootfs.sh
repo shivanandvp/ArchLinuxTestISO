d19e5b6f996de398b40727ea6cedfb06ccc5064b
shivanandvp <shivanandvp@users.noreply.github.com>
2018-11-21 05:41:17 -0600

    Automatic Login

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -29,29 +29,25 @@ cat>/etc/lightdm/lightdm.conf<< EOL
 run-directory=/run/lightdm
 
 [Seat:*]
-pam-service=lightdm
-pam-autologin-service=lightdm-autologin
 session-wrapper=/etc/lightdm/Xsession
+autologin-guest=false
 autologin-user=root
 autologin-user-timeout=0
+greeter-session = lightdm-webkit2-greeter
+greeter-show-manual-login=false
+greeter-hide-users=true
+allow-guest=false
+user-session=budgie-desktop
+
+[XDMCPServer]
+
+[VNCServer]
+
 EOL
 groupadd -r autologin
+groupadd -r nopasswdlogin
 gpasswd -a root autologin
-cat>/etc/pam.d/lightdm-autologin<< EOL
-#%PAM-1.0
-auth        required    pam_env.so
-auth        required    pam_tally.so file=/var/log/faillog onerr=succeed
-auth        required    pam_shells.so
-auth        required    pam_nologin.so
-# auth        [success=1 default=ignore]  pam_succeed_if.so user ingroup autologin
-auth        required    pam_unix.so
-auth        required    pam_permit.so
--auth       optional    pam_gnome_keyring.so
-account     include     system-local-login
-password    include     system-local-login
-session     include     system-local-login
--session    optional    pam_gnome_keyring.so auto_start
-EOL
+gpasswd -a root nopasswdlogin
 
 systemctl set-default graphical.target
 systemctl enable lightdm
